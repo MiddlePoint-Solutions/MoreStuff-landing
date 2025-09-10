ec48f87134d227145755cd86e046391c42e79089
Joe3112 <jhsagiv@gmail.com>
2025-09-10 12:56:59 +0200

    Fix vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import autoprefixer from "autoprefixer";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/", // Use absolute base so assets are requested from /assets even on deep routes
   server: {
     port: 5173,
     open: true,
